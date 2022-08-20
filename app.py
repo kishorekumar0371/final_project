8dae6ffc70af5eaa1550e50e86e17887781ca2f5
kishorekumar0371 <2019pietcskishore522@poornima.org>
2022-08-20 23:22:51 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/about")
+def about():
+    return render_template("aboutus.html")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+@app.route("/ann")
+def ann():
+    return render_template("ann.html")
+@app.route("/cnn")
+def cnn():
+    return render_template("cnn.html")
+@app.route("/ml")
+def ml():
+    return render_template("ml.html")
+@app.route("/rnn")
+def rnn():
+    return render_template("rnn.html")
+
+app.run(debug = True)
